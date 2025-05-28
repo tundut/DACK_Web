b4bc174631c5396b729cd18e2aad50b1f755cb7a
tundut <ducnguyenphan05@gmail.com>
2025-05-29 01:13:18 +0700

    fix

--- Project/routes/handleRequest.js
+++ Project/routes/handleRequest.js
@@ -3,17 +3,17 @@ const { pageRouter } = require('./pageRouter');
 const authRouter = require('./authRouter');
 const ordersRouter = require('./ordersRouter');
 const addRouter = require('./addRouter');
+const productRouter = require('./productRouter');
 const session = require('../config/session');
 
 const handleRequest = (req, res) => {
     session(req, res, () => {
         if (staticRouter(req, res)) return;
-        if (productRouter(req, res)) return;
         if (pageRouter(req, res)) return;
+        if (productRouter(req, res)) return;
         if (authRouter(req, res)) return;
         if (addRouter(req, res)) return;
         if (ordersRouter(req, res)) return;
-
 
         res.writeHead(404);
         res.end('Không tìm thấy trang!');
