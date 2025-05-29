c1db953431ce6c907e70d0d4706961319152326d
tundut <ducnguyenphan05@gmail.com>
2025-05-30 00:16:49 +0700

    fix swiper

--- Project/server.js
+++ Project/server.js
@@ -3,7 +3,7 @@ const handleRequest = require('./routes/handleRequest.js'); // Import file đị
 
 const server = http.createServer(handleRequest);
 
-const PORT = 3000;
+const PORT = 3001;
 server.listen(PORT, () => {
     console.log(`Server đang chạy tại http://localhost:${PORT}`);
 });
