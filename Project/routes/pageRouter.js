3aca9663c2ac0ac88180895ec12f35a2fe5c2572
Ngyn05 <hnguyenhuynh926@gmail.com>
2025-05-29 16:10:57 +0700

    phanquyen

--- Project/routes/pageRouter.js
+++ Project/routes/pageRouter.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { VaiTro } = require('../utils/constants');
 
 const renderPage = (res, page, extraReplacements = {}) => {
     const headerPath = path.join(__dirname, '..', 'views', 'header.html');
@@ -50,8 +51,7 @@ const pageRouter = (req, res) => {
                     <li><a class="dropdown-item" href="#">Profile</a></li>
                     <li><a class="dropdown-item" href="/logout">Logout</a></li>
                 </ul>
-            </div>
-        `
+            </div>`
         : `<a href="/login" class="ms-2 text-decoration-none text-dark fw-bold">Login</a>`;
 
         
@@ -71,6 +71,10 @@ const pageRouter = (req, res) => {
         return true;
     }
     if (url === '/cart' && req.method === 'GET') {
+        // Nếu là nhân viên thì không làm gì (không render, không alert, không chuyển trang)
+        if (req.session.user && req.session.user.vai_tro === VaiTro.NHAN_VIEN) {
+            return true;
+        }
         renderPage(res, 'cart.html', { ten_dang_nhap: ten_dang_nhap });
         return true;
     }
@@ -86,15 +90,25 @@ const pageRouter = (req, res) => {
     }
 
     if (url === '/orders' && req.method === 'GET') {
+        // Nếu là nhân viên thì không làm gì (không render, không alert, không chuyển trang)
+        if (req.session.user && req.session.user.vai_tro === VaiTro.NHAN_VIEN) {
+            return true;
+        }
         renderPage(res, 'orders.html', { ten_dang_nhap: ten_dang_nhap });
         return true;
     }
 
     if ((url === '/admin') && req.method === 'GET') {
+        // Phân quyền: chỉ cho phép nhân viên truy cập trang admin
+        if (!req.session.user || req.session.user.vai_tro !== VaiTro.NHAN_VIEN) {
+            // Nếu là khách hàng thì báo alert và chuyển về home
+            res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
+            res.end(`<script>alert('Bạn không có quyền truy cập trang này!');window.location.href='/home';</script>`);
+            return true;
+        }
         renderPage(res, 'manage.html', { ten_dang_nhap: ten_dang_nhap });
         return true;
     }
-
     return false;
     
 };
